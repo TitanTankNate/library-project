a3d154c314ae3b1152bcf68f60cbb2bfa63e9343
Nathan Janota <92643917+TitanTankNate@users.noreply.github.com>
2024-10-15 09:33:09 -0400

    Add boilerplate and initial html
    + Added boilerplate html
    + Added initil HTML structure
    + Pseudocoded initial JS plan
    + Copied in rough thematic style

---
+++ javascript.js
@@ -0,0 +1,14 @@
+// DESIRED BEHAVIOR
+// When user loads into page, a "default" template book should be 
+// displayed.  There will then be a "Add First Book" button that allows
+// the user to create their first book.
+
+// BOOK CARDS
+// Each book card should have the title, in header format, author, in 
+// sub-header format, number of pages, and a checkbox for whether or 
+// not the book has been read.  There should also be a delete button 
+// to allow the user to delete thebook.
+
+// DISPLAYING BOOK CARDS
+// The books should all display in card format with the elements listed
+// above.
