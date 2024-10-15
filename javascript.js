4a2257535a719f491bc0c5adedebc77c0a3a44d8
Nathan Janota <92643917+TitanTankNate@users.noreply.github.com>
2024-10-15 14:59:59 -0400

    Modify JS functions for efficiency
    ~ Tweaked addBookToLibrary method to not include instantiation of
    event listeners (moved event listeners to global scope)

--- javascript.js
+++ javascript.js
@@ -1,13 +1,18 @@
 // GLOBAL VARIABLES
 const userLibrary = [];
-let counter = 1;
+let counter = 0;
 
 
 
 // GLOBAL QUERY SELECTORS
-let contentContainer = document.querySelector(".content-layout-container");
+const contentContainer = document.querySelector(".content-layout-container");
 
-
+const dialogBox = document.getElementById("book-info-dialog");
+const dialogForm = document.getElementById("dialog-form");
+const titleInput = document.getElementById("title-text");
+const authorInput = document.getElementById("author-text");
+const pageCountInput = document.getElementById("pagecount-number");
+const isReadInput = document.getElementById("is-read-checkbox");
 
 // CONSTRUCTORS
 function Book(id, title, author, pageCount, isRead, isDisplayed) {
@@ -34,7 +39,11 @@ Book.prototype.deleteBookFromLibrary = function() {
 // EVENT HANDLERS
 let addBookButton = document.querySelector(".add-book-button");
 addBookButton.addEventListener("click", () => {
-    addBookToLibrary();
+    // addBookToLibrary();
+
+    // Pop up modal dialog requesting information
+    console.log("Creating modal dialog.");
+    dialogBox.showModal();
 });
 
 let deleteBookButton = document.querySelector(".card-delete-button");
@@ -42,87 +51,101 @@ deleteBookButton.addEventListener("click", () => {
     alert("Deleted book.");
 });
 
+let dialogConfirmButton = document.getElementById("dialog-confirm");
+dialogConfirmButton.addEventListener("click", () => {
+    addBookToLibrary();
+});
+
+const cancelButton = document.getElementById("dialog-cancel");
+cancelButton.addEventListener("click", () => {
+    dialogBox.close();
+});
+
+
+
 
 
 // GLOBAL METHODS------GLOBAL METHODS------GLOBAL METHODS------GLOBALM
 
 // FUNCTION: addBookToLibrary
-// DESCRIPTION: This function pops up a modal dialog requesting book 
-// information from the user.  Once submitted, that data is used to 
-// instantiate a new "Book" instance, and visually update the page.
+// DESCRIPTION: This function constructs a new instance of the Book
+// object, pushes that data to the library array, and forces a visual
+// refresh of the webpage, while resetting the modal form.
 function addBookToLibrary() {
-    // Pop up modal dialog requesting information
-    createModalDialog();
-
-    let newBook = new Book(counter, `sample${counter}`, `sample${counter}`, 123, false, false);
-
+    // Constructor
+    let newBook = new Book(counter, titleInput.value, 
+        authorInput.value, 
+        pageCountInput.value, 
+        isReadInput.value, 
+        false);  
+    
+    // Push changes to the array
     userLibrary.push(newBook);
     console.log(userLibrary);
 
-    updateBookCards();
+    // Force visual refresh
+    updateBookCards(counter);
     counter++;
+
+    // Reset modal dialog
+    dialogForm.reset();
 };
 
-// FUNCTION:
-// DESCRIPTION: 
-function createModalDialog() {
-    console.log("Creating modal dialog.");
-};
+
 
 // FUNCTION: updateBookCards
-// DESCRIPTION:  This function iterates across the userLibrary array 
-// and dynamically creates DOM content cards for each "entry".
-function updateBookCards() {
-    userLibrary.forEach(refBook => {
-        // Only execute creation of "new books"
-        if (!refBook.isDisplayed) {
-            // console.log(`${refBook.title} by ${refBook.author}, ${refBook.pageCount} pages, isRead: ${refBook.isRead}`);  
-    
-            // Create card div
-            let newCardDiv = document.createElement("div");
-            newCardDiv.classList.add("content-card");
-    
-            // // Create title h3
-            let newCardTitle = document.createElement("h3");
-            newCardTitle.classList.add("card-title");
-            newCardTitle.textContent = refBook.title;
-    
-            // // Create author h4
-            let newCardAuthor = document.createElement("h4");
-            newCardAuthor.classList.add("card-author");
-            newCardAuthor.textContent = refBook.author;
-    
-            // // Create pages p 
-            let newCardPages = document.createElement("p");
-            newCardPages.classList.add("card-pagecount");
-            newCardPages.textContent = `${refBook.pageCount} pages`;
-    
-            // // Create "Is read" status p
-            let newCardIsRead = document.createElement("p");
-            newCardIsRead.classList.add("card-is-book-read");
-            if (refBook.isRead) {
-                newCardIsRead.textContent = "Completed";
-            } else {
-                newCardIsRead.textContent = "Incomplete";
-            }
-    
-            // // Create delete button
-            let newCardDeleteButton = document.createElement("button");
-            newCardDeleteButton.classList.add("card-delete-button");
-            newCardDeleteButton.textContent = "Delete This Book";
-    
-            // Append new elements
-            contentContainer.appendChild(newCardDiv);
-            newCardDiv.appendChild(newCardTitle);
-            newCardDiv.appendChild(newCardAuthor);
-            newCardDiv.appendChild(newCardPages);
-            newCardDiv.appendChild(newCardIsRead);
-            newCardDiv.appendChild(newCardDeleteButton);
-    
-            // Set flag to avoid card duplication
-            refBook.isDisplayed = true;    
+// DESCRIPTION:  This function adds a new dynamic DOM series of 
+// elements whenever a new instance of Book object is created.
+function updateBookCards(index) {
+    console.log("updateBookCards()");
+    // Only execute creation of "new books"
+    if (!userLibrary[index].isDisplayed) {
+        // console.log(`${refBook.title} by ${refBook.author}, ${refBook.pageCount} pages, isRead: ${refBook.isRead}`);  
+
+        // Create card div
+        let newCardDiv = document.createElement("div");
+        newCardDiv.classList.add("content-card");
+
+        // // Create title h3
+        let newCardTitle = document.createElement("h3");
+        newCardTitle.classList.add("card-title");
+        newCardTitle.textContent = userLibrary[index].title;
+
+        // // Create author h4
+        let newCardAuthor = document.createElement("h4");
+        newCardAuthor.classList.add("card-author");
+        newCardAuthor.textContent = userLibrary[index].author;
+
+        // // Create pages p 
+        let newCardPages = document.createElement("p");
+        newCardPages.classList.add("card-pagecount");
+        newCardPages.textContent = `${userLibrary[index].pageCount} pages`;
+
+        // // Create "Is read" status p
+        let newCardIsRead = document.createElement("p");
+        newCardIsRead.classList.add("card-is-book-read");
+        if (userLibrary[index].isRead) {
+            newCardIsRead.textContent = "Completed";
+        } else {
+            newCardIsRead.textContent = "Incomplete";
         }
-    });
+
+        // // Create delete button
+        let newCardDeleteButton = document.createElement("button");
+        newCardDeleteButton.classList.add("card-delete-button");
+        newCardDeleteButton.textContent = "Delete This Book";
+
+        // Append new elements
+        contentContainer.appendChild(newCardDiv);
+        newCardDiv.appendChild(newCardTitle);
+        newCardDiv.appendChild(newCardAuthor);
+        newCardDiv.appendChild(newCardPages);
+        newCardDiv.appendChild(newCardIsRead);
+        newCardDiv.appendChild(newCardDeleteButton);
+
+        // Set flag to avoid card duplication
+        userLibrary[index].isDisplayed = true;    
+    }
 };
 
 // DESIRED BEHAVIOR
